8f4f7ad12a28d93bb889bcae5ed2588910dbcb92
tens04 <tens04@13xw.local>
2016-08-22 15:24:41 +0800

    qwe

---
+++ wqey/wqey/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  wqey
+//
+//  Created by tens04 on 16/8/22.
+//  Copyright © 2016年 tens04. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ wqey/wqey/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  wqey
+//
+//  Created by tens04 on 16/8/22.
+//  Copyright © 2016年 tens04. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
